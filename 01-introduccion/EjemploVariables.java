cb4841cd5f15e6bb19ded5c0d4b1587c83dfa53d
Sara Corrales <soysaracorralesrol@gmail.com>
2025-06-13 17:18:35 +0200

    EjemploVariables

---
+++ 01-introduccion/EjemploVariables.java
@@ -0,0 +1,9 @@
+public class EjemploVariables {
+    public static void main(String[] args) {
+        // Declaración, inicialización y asignación
+        String mensaje = "¡Hola Mundo!";  // Declaración e inicialización
+        int x; // Declaración
+        x = 7;  // Inicialización
+        mensaje = "¡Adiós Mundo!"; // Asignación
+    }
+}
